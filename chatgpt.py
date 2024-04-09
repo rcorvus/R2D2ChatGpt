4578eee9e8d8fb2150a5d8a2319d28b40c8a03df
rcorvus <5025458+rcorvus@users.noreply.github.com>
2024-04-09 11:32:27 -0700

    updated temperature

--- chatgpt.py
+++ chatgpt.py
@@ -19,12 +19,15 @@ def get_response(incoming_msg):
     try:
         client = OpenAI(api_key=api_key)        
     
+        # temperature=0 did result in much more consistent answers in general, but much less creativity and variety on other responses.  
+        # It produced more accurate responses about his age but if he got the answer wrong the first time, he would always get it wrong the rest of the conversation.
         response = client.chat.completions.create(
-            model="gpt-3.5-turbo", 
+            model="gpt-3.5-turbo",
+            temperature = 0.7,             
             messages=messages)
-        content = response.choices[0].message.content
-        data.append({"role": "assistant", "content": content})
-        return content
+        response_content = response.choices[0].message.content
+        data.append({"role": "assistant", "content": response_content})
+        return response_content
     except BaseException as error:
         print(error)
         return error
